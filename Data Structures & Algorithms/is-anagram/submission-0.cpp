8a2074856037b02c4248d4d13ca62e1b508574ae
TruongHoMinhThuan <2528000383@student.hcmus.edu.vn>
2026-09-06 17:28:45 +0700

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n = s.length();
+        int m = t.length();
+        if (n != m) return false;
+        int arr1[26] = {0};
+        int arr2[26] = {0};
+        for (int i=0; i<n; i++){
+            arr1[s[i]-97] += 1;
+            arr2[t[i]-97] += 1;
+        }
+        for (int i=0; i<26; i++){
+            if (arr1[i] != arr2[i]) return false;
+        }
+        return true;
+    }
+};
